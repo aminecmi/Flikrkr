b32eb6092cf49d0a63d79d7a279fca30c448a7ba
makowildcat <makowildcat@gmail.com>
2014-04-10 09:56:56 +0200

    Initial Commit

---
+++ Flikrkr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Flikrkr
+//
+//  Created by esgi on 10/04/2014.
+//  Copyright (c) 2014 Oui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FlikrkrTests/FlikrkrTests.m
@@ -0,0 +1,34 @@
+//
+//  FlikrkrTests.m
+//  FlikrkrTests
+//
+//  Created by esgi on 10/04/2014.
+//  Copyright (c) 2014 Oui. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FlikrkrTests : XCTestCase
+
+@end
+
+@implementation FlikrkrTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
